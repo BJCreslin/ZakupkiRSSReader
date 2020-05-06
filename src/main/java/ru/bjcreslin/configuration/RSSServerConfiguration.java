952006bdf62df28253c6d99033a3488350acb369
bjcreslin <bjcreslin@gmail.com>
2020-05-06 20:42:56 +0700

    configuration

---
+++ src/main/java/ru/bjcreslin/configuration/RSSServerConfiguration.java
@@ -0,0 +1,6 @@
+package ru.bjcreslin.configuration;
+
+public interface RSSServerConfiguration {
+    String QUERY_STRING="https://zakupki.gov.ru/tinyurl/8f04c559-63d1-4629-8e42-03b3d327dd82";
+    String USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0";
+}
