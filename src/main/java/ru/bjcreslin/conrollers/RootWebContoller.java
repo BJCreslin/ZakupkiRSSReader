9634d68e9ad317ba4de3193bd66caecfd3269d96
bjcreslin <bjcreslin@gmail.com>
2020-05-18 16:27:58 +0700

    Добавил Swagger2

--- src/main/java/ru/bjcreslin/conrollers/RootWebContoller.java
+++ src/main/java/ru/bjcreslin/conrollers/RootWebContoller.java
@@ -1,18 +1,17 @@
 package ru.bjcreslin.conrollers;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+@Api
 @Controller
 @RequestMapping("/")
 public class RootWebContoller {
 
     @GetMapping("/")
-    @ResponseBody
     public String getIndex() {
-        var answer = "Answer";
-        return (answer);
+        return "/swagger-ui.html";
     }
 }
