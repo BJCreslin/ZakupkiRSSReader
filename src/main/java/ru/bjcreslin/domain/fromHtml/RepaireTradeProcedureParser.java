123a206679ac6075f8d7ffc6d448a4ae7e352412
bjcreslin <bjcreslin@gmail.com>
2020-06-08 00:33:44 +0700

    пределка

--- src/main/java/ru/bjcreslin/domain/fromHtml/RepaireTradeProcedureParser.java
+++ src/main/java/ru/bjcreslin/domain/fromHtml/RepaireTradeProcedureParser.java
@@ -15,52 +15,46 @@ import java.io.IOException;
  */
 @Log
 public class RepaireTradeProcedureParser {
+    private ProcedureDispatcher dispatcher = new ProcedureDispatcher();
 
     public ProcedureFromHtmlParser getResult(String uin) {
-        String requestedAddress = RepairsServerConfiguration.ADRESS_OBJECT_FROM_SERVER_BY_UIN + uin;
+        String requestedAddress = getRequestedAddressFromUin(uin);
         var tradeProcedure = new ProcedureFromHtmlParser();
         try {
-            var document = Jsoup.connect(requestedAddress)
-                    .userAgent(Constants.USER_AGENT).get();
-            var elements = document
-                    .body()
-                    .getElementsByTag("td")
-                    .html()
-                    .replace("</p>", "")
-                    .split("(<p class=\"parameter\">)|(<p class=\"parameterValue\">)");
-
-            ProcedureFromHtmlParser procedure = new ProcedureFromHtmlParser();
-            for (int i = 0; i < elements.length; i++) {
-                if (elements[i].equals("Номер закупки")) {
-                    procedure.setNumber(elements[++i]);
-                }
-                if (elements[i].equals("Наименование закупки")) {
-                    procedure.setName(elements[i++]);
-                }
-
-                if (elements[i].equals("Наименование электронной площадки")) {
-                    procedure.setName(elements[i++]);
-                }
-
-
-            }
-            var dispatcher = new ProcedureDispatcher();
-
-            for (int i = 0; i < elements.length; i++) {
-                elements[i]=elements[i].trim();
-             //   System.out.println(i+" :"+elements[i]);
-                if (dispatcher.getMap().containsKey(elements[i].trim())) {
-                    dispatcher.getMap().get(elements[i]).
-                            accept(tradeProcedure, elements[++i]);
-                }
-
-            }
-
-
+            String[] elements = getLinesFromAddress(requestedAddress);
+            tradeProcedure = makeProcedureFromLines(dispatcher, elements);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return tradeProcedure;
+    }
+
+    private ProcedureFromHtmlParser makeProcedureFromLines(ProcedureDispatcher dispatcher, String[] elements) {
+        var tradeProcedure = new ProcedureFromHtmlParser();
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = elements[i].trim();
+            //   System.out.println(i+" :"+elements[i]);
+            if (dispatcher.getMap().containsKey(elements[i].trim())) {
+                dispatcher.getMap().get(elements[i]).
+                        accept(tradeProcedure, elements[++i]);
+            }
+        }
+        return tradeProcedure;
+    }
+
+    private String[] getLinesFromAddress(String requestedAddress) throws IOException {
+        var document = Jsoup.connect(requestedAddress)
+                .userAgent(Constants.USER_AGENT).get();
+        return document
+                .body()
+                .getElementsByTag("td")
+                .html()
+                .replace("</p>", "")
+                .split("(<p class=\"parameter\">)|(<p class=\"parameterValue\">)");
+    }
+
+    private String getRequestedAddressFromUin(String uin) {
+        return RepairsServerConfiguration.ADRESS_OBJECT_FROM_SERVER_BY_UIN + uin;
     }
 
 
