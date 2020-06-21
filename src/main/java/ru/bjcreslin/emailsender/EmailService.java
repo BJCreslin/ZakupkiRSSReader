b91742514a53519ab8425564c74670e9d8fdda3f
bjcreslin <bjcreslin@gmail.com>
2020-06-21 21:57:35 +0700

    email sending cintroller

--- src/main/java/ru/bjcreslin/emailsender/EmailService.java
+++ src/main/java/ru/bjcreslin/emailsender/EmailService.java
@@ -1,4 +1,6 @@
 package ru.bjcreslin.emailsender;
 
 public interface EmailService {
+    public void sendSimpleMessage(
+            String to, String subject, String text);
 }
---
+++ src/main/java/ru/bjcreslin/emailsender/TestUserSender.java
@@ -0,0 +1,10 @@
+package ru.bjcreslin.emailsender;
+
+import lombok.Data;
+
+@Data
+public class TestUserSender {
+    private final String to="bjcreslin@gmail.com";
+    private final String subj="tester 12";
+    private final String text="This is test text";
+}
